57294f3bbd9456fe32bfaf7731188ffd1d045e8d
Kane <106299362+kane9028@users.noreply.github.com>
2022-06-24 22:10:15 -0700

    Edit script.js - Complete game

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ if(confirmGamePlay){
     alert("See you!")
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
 // Once the game starts, prompt the user to type in rock/paper/scissors
-    playerSelection = prompt("Please input your selection from rock/paper/scissors: ");
+    let playerSelection = prompt("Please input your selection from rock/paper/scissors: ");
     // if the user's input is invalid, push an error message and ask the user to input the selection again
     let selections = ["rock", "paper", "scissors"];
     while (!checkValidation(playerSelection.toLowerCase(), selections)) {
         playerSelection = prompt(`Your input, ${playerSelection}, is invalid. Please input your selection from rock/paper/scissors: `);
     } 
     // Once user inputs valid selection, let the computer generate a random selection from rock/paper/scissor
-    computerSelection = computerPlay();
+    let computerSelection = computerPlay();
     // Compare user's selection with computer's selection to get the result and alert user with result 
     let result = playerWon(playerSelection.toLowerCase(),computerSelection);
     result ? alert(`Your selection: ${playerSelection}\nComputer's selection: ${computerSelection}\nCongraduation! You won this round!`) : alert(`Your selection: ${playerSelection}\nComputer's selection: ${computerSelection}\nUnfortunatly, You lost this round.`);
@@ -27,8 +27,22 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
     // store each single round result into local memory
-    // Repeat playround until 5 rounds game are completed
-    // Alert user with the final result and reset the game
+    let result = {};
+    // Repeat playround until 5 rounds game are completed, store result to local memory
+    for(let i=0; i<5; i++){
+        alert(`Round ${i+1} of 5!`);
+        if(playRound()){
+            result.playerWonCount ? result.playerWonCount += 1 : result.playerWonCount = 1;
+        } else {
+            result.computerWenCount ? result.computerWenCount +=1 : result.computerWenCount = 1;
+        }
+    }
+    // Alert user with the final result and quit the game
+    alert(`After 5 round of gameplay, you have won ${result.playerWonCount} times, computer have won ${result.computerWenCount} times.`);
+    if( result.playerWonCount > result.computerWenCount ) alert(`Congraduation! You are the Final Winner!`);
+    if( result.playerWonCount < result.computerWenCount ) alert(`Sadly, You are the Final loser.`);
+    if( result.playerWonCount === result.computerWenCount ) alert(`Unbelievable! You made Five draw in a roll.`);
+
 }
 
 //declare a function to generate a random selection 
